8b7eeaee021cc14fef676391b455389a75938f8d
wank-it <n.nquanght@gmail.com>
2024-12-29 20:26:39 +0700

    config header request

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -5,35 +5,59 @@ const maxRetries = 10
 
 const callAPIService = async (url = '', headers = {}, method = 'get', data = {}) => {
     let attempt = 0
+    // let timeOut = 1000
+    let delayTimeOut = 500
     
-    while (true) {
-        try {
-            let options = {
-                method: method,
-                timeout: timeOut,
-                headers: headers,
-                data: data
-            }
+    // while (true) {
+    //     try {
+    //         let options = {
+    //             method: method,
+    //             timeout: timeOut,
+    //             headers: headers,
+    //             data: data
+    //         }
 
-            const response = await axios(url, options)
+    //         const response = await axios(url, options)
 
-            return response
-        } catch (error) {
-            if (error.response && error.response.status === statusCode.unAuthorized) {
-                console.error(`Status code: ${statusCode.unAuthorized} - Retrying fetch data`);
-            } else {
-                console.error(error.response.message)
-                throw error
-            }
+    //         return response
+    //     } catch (error) {
+    //         if (error.response && error.response.status === statusCode.unAuthorized) {
+    //             console.error(`Status code: ${statusCode.unAuthorized} - Retrying fetch data`);
+    //         } else {
+    //             console.error(error.response.message)
+    //             throw error
+    //         }
+    //     }
+
+    //     attempt += 1
+
+    //     if (attempt > maxRetries) {
+    //         throw new Error("Max retries reached. Request failed.");
+    //     }
+
+    //     await helper.sleep(timeOut) // delay 1s
+
+    //     timeOut += delayTimeOut
+    // }
+
+    try {
+        let options = {
+            method: method,
+            timeout: timeOut,
+            headers: headers,
+            data: data
         }
 
-        attempt += 1
+        const response = await axios(url, options)
 
-        if (attempt > maxRetries) {
-            throw new Error("Max retries reached. Request failed.");
+        return response
+    } catch (error) {
+        if (error.response && error.response.status === statusCode.unAuthorized) {
+            console.error(`Status code: ${statusCode.unAuthorized} - Retrying fetch data`);
+        } else {
+            console.error(error)
+            throw error
         }
-
-        await helper.sleep(1000) // delay 1s
     }
 }
 
